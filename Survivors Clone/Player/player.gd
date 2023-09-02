33a0e8a8770746346db684e4c0f01247cfa78714
diegoyamasaki <yamasaki.diego@gmail.com>
2023-09-02 15:09:31 -0300

    aula 9

--- Survivors Clone/Player/player.gd
+++ Survivors Clone/Player/player.gd
@@ -8,6 +8,7 @@ var last_movement = Vector2.UP
 #Attack
 var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
 var tornado = preload("res://Player/Attack/tornado.tscn")
+var javalin = preload("res://Player/Attack/javalin.tscn")
 
 #AttackNode
 @onready var iceSpearTimer:Timer = get_node("%IceSpearTimer")
@@ -16,6 +17,8 @@ var tornado = preload("res://Player/Attack/tornado.tscn")
 @onready var tornadoTimer:Timer = get_node("%TornadoTimer")
 @onready var tornadoAttackTimer:Timer = get_node("%TornadoAttackTimer")
 
+@onready var javalintBase = get_node("%javalinBase")
+
 #IceSpear
 var icespear_ammo = 0
 var icespear_baseammo = 0
@@ -23,10 +26,15 @@ var icespear_attackspeed = 1.5
 var icespear_level = 0
 
 #Tornado
-var tornado_ammo = 0
-var tornado_baseammo = 5
+var tornado_ammo = 1
+var tornado_baseammo = 1
 var tornado_attackspeed = 3
 var tornado_level = 1
+
+
+#Tornado
+var javalin_ammo = 1
+var javalin_level = 1
 
 #Enemy Related
 var enemy_close = []
@@ -74,6 +82,8 @@ func attack():
 		tornadoTimer.wait_time = tornado_attackspeed
 		if tornadoTimer.is_stopped():
 			tornadoTimer.start()
+	if javalin_level > 0:
+		spam_javalin()
 
 
 func _on_ice_spear_timer_timeout():
@@ -130,3 +140,12 @@ func _on_tornado_attack_timer_timeout():
 			tornadoAttackTimer.start()
 		else:
 			tornadoAttackTimer.stop()
+
+func spam_javalin():
+	var get_javalin_total = javalintBase.get_child_count()
+	var calc_spams = javalin_ammo - get_javalin_total
+	while calc_spams > 0:
+		var javalin_spam = javalin.instantiate()
+		javalin_spam.global_position = global_position
+		javalintBase.add_child(javalin_spam)
+		calc_spams -= 1
